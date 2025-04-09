5ce3aa7c83c7aaeb5138073a8239a208be5fafd2
TurtleHow <160984504+TurtleHow@users.noreply.github.com>
2025-04-08 23:05:20 -0400

    Update shopper_manage.js

--- js/shopper_manage.js
+++ js/shopper_manage.js
@@ -1,16 +1,22 @@
 let JSONObject = {
-    "shopper": ["", "", "", "", "", ""]
+    "shopper": {
+        "email": "",
+        "firstName": "",
+        "lastName": "",
+        "phone": "",
+        "age": "",
+        "address": ""
+    }
 };
 
+
 function fillout() {
-    if (!JSONObject == null) {
         document.getElementById("email").value = JSONObject.shopper[0].email;
         document.getElementById("first_name").value = JSONObject.shopper[0].firstName;
         document.getElementById("last_name").value = JSONObject.shopper[0].lastName;
         document.getElementById("phone").value = JSONObject.shopper[0].phone;
         document.getElementById("age").value = JSONObject.shopper[0].age;
         document.getElementById("address").value = JSONObject.shopper[0].address;
-    }
 }
 
 function makeObject(email, first_name, last_name, phone, age, address) {
