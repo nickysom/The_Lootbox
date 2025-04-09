96719f4a7206f569ebda47fc8d243184b6852beb
TurtleHow <160984504+TurtleHow@users.noreply.github.com>
2025-04-08 20:38:35 -0400

    Update shopper_manage.js

--- js/shopper_manage.js
+++ js/shopper_manage.js
@@ -1,27 +1,28 @@
-let var JSONObject = {};
+let JSONObject = {};
 
-	function fillout()
-            {
-                if (!JSONObject == null)
-                {
-                    document.getElementById("email").value = JSONObject.shopper[0].email;
-                    document.getElementById("first_name").value = JSONObject.shopper[0].firstName;
-                    document.getElementById("last_name").value = JSONObject.shopper[0].lastName;
-                    document.getElementById("phone").value = JSONObject.shopper[0].phone;
-                    document.getElementById("age").value = JSONObject.shopper[0].age;
-                    document.getElementById("address").value = JSONObject.shopper[0].address;
-                }
-            }
+function fillout()
+{
+if (!JSONObject == null)
+	{
+	    document.getElementById("email").value = JSONObject.shopper[0].email;
+	    document.getElementById("first_name").value = JSONObject.shopper[0].firstName;
+	    document.getElementById("last_name").value = JSONObject.shopper[0].lastName;
+	    document.getElementById("phone").value = JSONObject.shopper[0].phone;
+	    document.getElementById("age").value = JSONObject.shopper[0].age;
+	    document.getElementById("address").value = JSONObject.shopper[0].address;
+	}
+}
 
-  function makeObject(email, first_name, last_name, phone, age, address)
-      {
-          JSONObject = {"shopper" : [
-    {
-              "email": document.getElementById('email').value,
-              "firstName": document.getElementById('first-name').value,
-              "lastName": document.getElementById('last-name').value,
-              "phone": document.getElementById('phone').value,
-              "age": document.getElementById('age').value,
-              "address": document.getElementById('address').value,
-    }]
-  }
+function makeObject(email, first_name, last_name, phone, age, address)
+{
+	JSONObject = {"shopper" : [
+		{
+		      "email": document.getElementById('email').value,
+		      "firstName": document.getElementById('first-name').value,
+		      "lastName": document.getElementById('last-name').value,
+		      "phone": document.getElementById('phone').value,
+		      "age": document.getElementById('age').value,
+		      "address": document.getElementById('address').value,
+		}
+	]}
+}
