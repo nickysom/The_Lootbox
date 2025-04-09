615006780927153191cad449fbae51b04d289fa2
TurtleHow <160984504+TurtleHow@users.noreply.github.com>
2025-04-09 13:52:46 -0400

    Update shopper_manage.js

--- js/shopper_manage.js
+++ js/shopper_manage.js
@@ -25,7 +25,10 @@ function validateMakeObject(email, first_name, last_name, phone, age, address) {
 		alert("Email must be valid.");
 		return false;
 	}
-    if (!/^\(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(phone))
+    if (!^[1-9]\d{2}-\d{3}-\d{4}.test(phone) ||
+	!^\(\d{3}\)\s\d{3}-\d{4}.test(phone) ||
+	!^[1-9]\d{2}\s\d{3}\s\d{4}.test(phone) ||
+	!^[1-9]\d{2}\.\d{3}\.\d{4}.test(phone))
 	{
 		alert("Phone must be valid.");
 		return false;
